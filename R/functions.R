c2e79dc87f542a8c3ad45e88b8583388c268e3d0
Julie Bjerrelund <jbjerrelund@health.sdu.dk>
2026-05-06 10:34:59 +0200

    Made a function to read in all data and combine the rows. Transferred the testet code into R.script

--- R/functions.R
+++ R/functions.R
@@ -15,3 +15,13 @@ read <- function(file_path, max_rows = 100) { # default argument, so if we dont 
   return(data)
 }
 
+
+read_all <- function(filename) {
+  files <- here::here("data-raw/nurses-stress/") %>%
+    fs::dir_ls(regexp = filename, recurse = TRUE)
+
+  data <- files %>%
+    purrr::map(read) %>%
+    purrr::list_rbind(names_to = "file_path_id")
+  return(read_all)
+}
